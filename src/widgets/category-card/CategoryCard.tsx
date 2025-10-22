9218bac6ca13336000f0a881b9c447ba243cf12e
alex <sobol4156@gmail.com>
2025-10-23 00:10:33 +0300

    feat: added more mock products and; style: correct image styles

--- src/widgets/category-card/CategoryCard.tsx
+++ src/widgets/category-card/CategoryCard.tsx
@@ -7,12 +7,13 @@ interface CategoryProps {
 
 export default function CategoryCard({ category }: CategoryProps) {
   return (
-    <div className="bg-white p-4 transition-colors ease-in-out rounded-[30px] flex shrink-0 flex-col gap-2.5 cursor-pointer hover:bg-flash-white">
+    <div className="bg-white p-4 transition-colors ease-in-out rounded-[30px] shrink-0 flex flex-col gap-2.5 cursor-pointer hover:bg-flash-white">
       <Image
         src={category.image}
         width={65}
         height={65}
         priority
+        style={{ width: 65, height: 65, objectFit: "contain" }}
         className="rounded-full" alt="category" />
       <p className="xtra-small-bold text-center">{category.name}</p>
     </div>
