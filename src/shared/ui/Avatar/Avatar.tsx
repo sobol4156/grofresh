0d228f6d41e3bc3a4e7714add4c8402da2586783
alex <sobol4156@gmail.com>
2025-11-01 00:01:11 +0300

    feat: added default photo

--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -5,6 +5,6 @@ interface Props {
   size?: number
 }
 
-export default function Avatar({ src = 'https://avatars.mds.yandex.net/get-entity_search/2050844/1017842602/S600xU_2x', size = 50 }: Props) {
+export default function Avatar({ src = '/images/user-no-photo.png', size = 50 }: Props) {
   return <AvatarMui alt="Remy Sharp" src={src} sx={{ width: size, height: size }} />
 }
